fc9f5063461cef482ec50a9626d05af0fed770de
motoki3150 <108526062+motoki3150@users.noreply.github.com>
2022-08-19 03:56:03 +0000

    [fin]nagano_cake

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,30 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'orders/show'
+  end
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+  end
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/edit'
+  end
+  namespace :admin do
+    get 'items/index'
+    get 'items/new'
+    get 'items/show'
+    get 'items/edit'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
+  get 'items/index'
+  get 'items/show'
+  get 'homes/top'
+  get 'homes/about'
+  devise_for :customers
+  devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20220819025814_create_oder_details.rb
@@ -0,0 +1,11 @@
+class CreateOderDetails < ActiveRecord::Migration[6.1]
+  def change
+    create_table :oder_details do |t|
+      t.integer :price, null: false
+      t.integer :amount, null: false
+      t.integer :making_status, null: false, default: 0
+
+      t.timestamps
+    end
+  end
+end
